dc96f7087574a9ee6bc81f70bfed60f3b0c8769a
Hassan Roland <roland.nasser@agroscope.admin.ch>
2022-12-08 16:36:57 +0000

    OD code updated

---
+++ 1_object_detection/upload_azcopy.sh
@@ -0,0 +1,3 @@
+azcopy copy "images" "https://upskillingwork0835328750.blob.core.windows.net/azureml-blobstore-f60d6c22-fab8-43c7-b0e1-977809a11191/rumex_labelbox?sp=racwdli&st=2022-12-08T15:50:40Z&se=2022-12-08T23:50:40Z&spr=https&sv=2021-06-08&sr=c&sig=AKzeWMS%2Bpdw88%2Bv9laYaSbgnMuNAleMz0wceajOF7D8%3D" --recursive
+
+
